16a17451e0c431d8b6c96ec5b2233482b8f3c807
Jorge Garcia <jorgemgr94@gmail.com>
2019-08-29 01:53:14 -0500

    Remove unwanted stuff

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./containers/App/";
 import * as serviceWorker from "./serviceWorker";
+
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap/dist/js/bootstrap.bundle";
 
