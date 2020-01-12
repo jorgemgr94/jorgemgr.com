14773b1a73215e77df4267d0a7a2fb7c8f957073
Jorge Marcial Garcia Rizo <jorge.garcia@sintec.com>
2020-01-11 19:54:01 -0600

    Skills & Experience add database

--- src/containers/App/Skills.js
+++ src/containers/App/Skills.js
@@ -1,36 +1,31 @@
 import React from "react";
+import { technologies } from "../../util/database";
 
 function Skills() {
-	return (
-		<section
-			className="resume-section p-3 p-lg-5 d-flex align-items-center"
-			id="skills"
-		>
-			<div className="w-100">
-				<h2 className="mb-5">Skills</h2>
-				<h3 className="mb-2">Current dev stack</h3>
-				<ul className="mb-4">
-					<li>React, Javascript, Typescript</li>
-					<li>NodeJs, Ruby on Rails</li>
-					<li>PostgreSQL, MongoDb, Redis</li>
-					<li>React Native</li>
-					<li>Gitlab, AWS, Heroku, Docker, Eslint</li>
-				</ul>
-				<h3 className="mb-2">
-					Things I'm learning or I'm looking to learn soon
-				</h3>
-				<p className="mb-4">
-					Go, Jest, Terraform, TypeORM, Gitlab CI / CD, Kubernetes, GraphQl,
-					Devops, Django, Electron, Rust, Dart, Elixir, Web Assembly.
-				</p>
-				<h3 className="mb-2">Technologies I have used</h3>
-				<p className="mb-4">
-					Python, Laravel, VueJS, Php, Html5, CSS3, Android w/ Java, Android w/
-					Kotlin, IOS w/ Swift, MySql, MariaDB, SQL Server, Google Cloud, Bash.
-				</p>
-			</div>
-		</section>
-	);
+  return (
+    <section
+      className="resume-section p-3 p-lg-5 d-flex align-items-center"
+      id="skills"
+    >
+      <div className="w-100">
+        <h2 className="mb-5">Skills</h2>
+        <h3 className="mb-2">Current dev stack</h3>
+        <p className="mb-4">
+          {technologies.filter(t => t.status === 0).map(t => `${t.name}, `)}
+        </p>
+        <h3 className="mb-2">
+          Things I'm learning or I'm looking to learn soon
+        </h3>
+        <p className="mb-4">
+          {technologies.filter(t => t.status === 1).map(t => `${t.name}, `)}
+        </p>
+        <h3 className="mb-2">Technologies I have used</h3>
+        <p className="mb-4">
+          {technologies.filter(t => t.status === 2).map(t => `${t.name}, `)}
+        </p>
+      </div>
+    </section>
+  );
 }
 
 export default Skills;
---
+++ src/util/database.js
@@ -0,0 +1,97 @@
+export const technologies = [
+  { name: "React", status: 0 },
+  { name: "Javascript", status: 0 },
+  { name: "Typescript", status: 0 },
+  { name: "NodeJs", status: 0 },
+  { name: "PostgreSQL", status: 0 },
+  { name: "MongoDb", status: 0 },
+  { name: "React Native", status: 0 },
+  { name: "Gitlab", status: 0 },
+  { name: "AWS", status: 0 },
+  { name: "Heroku", status: 0 },
+  { name: "Docker", status: 0 },
+  { name: "Eslint", status: 0 },
+  { name: "Google Cloud", status: 0 },
+  { name: "Python", status: 0 },
+
+  { name: "Go", status: 1 },
+  { name: "Jest", status: 1 },
+  { name: "Terraform", status: 1 },
+  { name: "TypeORM", status: 1 },
+  { name: "Gitlab CI / CD", status: 1 },
+  { name: "Kubernetes", status: 1 },
+  { name: "GraphQl", status: 1 },
+  { name: "Devops", status: 1 },
+  { name: "Django", status: 1 },
+  { name: "Electron", status: 1 },
+  { name: "Rust", status: 1 },
+  { name: "Dart", status: 1 },
+  { name: "Elixir", status: 1 },
+  { name: "Web Assembly", status: 1 },
+
+  { name: "Laravel", status: 2 },
+  { name: "VueJS", status: 2 },
+  { name: "Php", status: 2 },
+  { name: "Html5", status: 2 },
+  { name: "CSS3", status: 2 },
+  { name: "Android w/ Java", status: 2 },
+  { name: "Android w/Kotlin", status: 2 },
+  { name: "IOS w/ Swift", status: 2 },
+  { name: "MySQL", status: 2 },
+  { name: "MariaDB", status: 2 },
+  { name: "SQL Server", status: 2 },
+  { name: "Bash", status: 2 },
+  { name: "Redis", status: 2 },
+  { name: "Ruby on Rails", status: 2 }
+];
+
+export const positions = [
+  {
+    name: "Consultant / Full Stack Developer",
+    company: "SINTEC",
+    startAt: new Date("02/01/2019"),
+    endAt: null,
+    description:
+      "Technical Environment: Php, NodeJs, Javascript, ReactJs, React Native, MySQL, MongoDb, Google Cloud, AWS, Heroku, Git, Docker,"
+  },
+  {
+    name: "Full Stack Developer (Remote, Part time)",
+    company: "Garage (Startup)",
+    startAt: new Date("07/01/2019"),
+    endAt: new Date("12/31/2019"),
+    description:
+      "Technical Environment: Ruby on Rails, Javascript, ReactJs, PostgreSQL, Heroku, Git, SCRUM, Docker, Gitlab,"
+  },
+  {
+    name: "Full Stack Developer",
+    company: "X8bit / SENSAI Empower Success (Startup) / Metalsa",
+    startAt: new Date("06/01/2018"),
+    endAt: new Date("02/01/2019"),
+    description:
+      "Technical Environment: NodeJs, Ruby on Rails, Javascript, ReactJs, React Native, MySQL, SQL Server, AWS, Git, SCRUM, Linux server administration, Jira,"
+  },
+  {
+    name: "Team Leader",
+    company: "HMS Sistemas de Información / ONCARE",
+    startAt: new Date("10/01/2016"),
+    endAt: new Date("08/01/2018"),
+    description:
+      "Technical Environment: Php, Javascript, ReactJs, Android w/ Kotlin, IOS w/ Swift, MySQL, AWS, Git, Linux server administration, Bash,"
+  },
+  {
+    name: "Software Developer",
+    company: "HMS Sistemas de Información / ONCARE",
+    startAt: new Date("01/01/2015"),
+    endAt: new Date("10/01/2016"),
+    description:
+      "Technical Environment: Php, Javascript, MySQL, AWS, Linux server administration, Bash,"
+  },
+  {
+    name: "Software Developer",
+    company: "Ingeniat",
+    startAt: new Date("06/01/2014"),
+    endAt: new Date("01/01/2015"),
+    description:
+      "Technical Environment: Php, Javascript, Android w/ Java, MySQL, Linux server administration, Bash,"
+  }
+];
