2442d5990663ab7207ce1208a07b251be79da7be
Jorge García <jorge.garcia@sintec.com>
2021-06-23 10:43:09 -0500

    Change about section

--- src/containers/Website/About.js
+++ src/containers/Website/About.js
@@ -3,34 +3,34 @@ import { NavLink } from "react-router-dom";
 
 import { socialIcons, aptitude } from "../../util/database";
 
-function About () {
+function About() {
 	return (
 		<section
-			className='resume-section p-3 p-lg-5 d-flex align-items-center'
-			id='about'
+			className="resume-section p-3 p-lg-5 d-flex align-items-center"
+			id="about"
 		>
-			<div className='w-100'>
-				<h1>Hi, I'm Jorge García!</h1>
-				<div className='subheading mb-5'>
+			<div className="w-100">
+				<h1>Hi, I'm Jorge García! 👨‍💻</h1>
+				{/* <div className='subheading mb-5'>
 					<span role='img' aria-label='intro'>
 						Software Engineer 👨‍💻, Consultant 🏢, Geek 🤓, Gamer 🎮, Startups
 						lover 🚀 and more..
 					</span>
-				</div>
-				<p className='lead mb-5'>{aptitude}</p>
-				<div className='social-icons'>
-					{socialIcons.map(socialIcon => (
+				</div> */}
+				<p className="lead mb-5">{aptitude}</p>
+				<div className="social-icons">
+					{socialIcons.map((socialIcon) => (
 						<a
 							key={socialIcon.id}
 							href={socialIcon.url}
-							target='_blank'
-							rel='noopener noreferrer'
+							target="_blank"
+							rel="noopener noreferrer"
 						>
 							<i className={socialIcon.icon}></i>
 						</a>
 					))}
-					<NavLink to='/resume' target='_blank'>
-						<i className='far fa-file-pdf'></i>
+					<NavLink to="/resume" target="_blank">
+						<i className="far fa-file-pdf"></i>
 					</NavLink>
 				</div>
 			</div>
